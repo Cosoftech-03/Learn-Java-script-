326d1ed560c650330d549148ccf0a25b172bfa8e
arvindkri-03 <arvindkriyadav03@gmail.com>
2024-05-27 03:09:40 +0530

    mathmiatical operations

---
+++ 01_Basics/number_Maths.js
@@ -0,0 +1,7 @@
+const score=400
+console.log(score);
+
+const balance=new Number(100)   //Number keyword is used for specially defined numbers.
+console.log(balance);
+
+console.log(balance.tostring());
